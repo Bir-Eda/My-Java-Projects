c1de2577bd69228d5ab1287c511dd8f4f320ae6c
Bir-Eda <60522150+Bir-Eda@users.noreply.github.com>
2020-05-14 18:22:18 -0400

    My Commits

---
+++ src/finalreview/LoopsExample.java
@@ -0,0 +1,13 @@
+package finalreview;
+
+public class LoopsExample {
+    public static void main(String[] args) {
+       // for((int i=0, j=10); (i<10 && j>=0); (i++, j--)){
+         //   1-initilazation 2- condition 3-inc/dec
+
+
+           // System.out.println(i);
+        }
+
+    }
+
---
+++ src/finalreview/StringReversing.java
@@ -0,0 +1,10 @@
+package finalreview;
+
+public class StringReversing {
+    public static void main(String[] args) {
+        String str="Java is fun";
+        for(int i=str.length()-1; i>=0; i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
